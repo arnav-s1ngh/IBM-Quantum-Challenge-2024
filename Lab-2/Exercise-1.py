7206217afba975744ecf3baa27ad4f7f74bd5053
Arnav Singh <92050142+arnav-s1ngh@users.noreply.github.com>
2024-06-12 19:09:33 +0530

    Update Exercise-1.py

--- Lab-2/Exercise-1.py
+++ Lab-2/Exercise-1.py
@@ -1,1 +1,42 @@
-#
+import matplotlib.pyplot as plt
+from qiskit import QuantumCircuit, transpile
+import os
+os.environ['QXToken'] = "Paste_Your_Token_Here"
+import numpy as np
+from typing import List, Callable
+from scipy.optimize import minimize
+from scipy.optimize._optimize import OptimizeResult
+import matplotlib.pyplot as plt
+
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import Statevector, Operator, SparsePauliOp
+from qiskit.primitives import StatevectorSampler, PrimitiveJob
+from qiskit.circuit.library import TwoLocal
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit.visualization import plot_histogram
+from qiskit_ibm_runtime.fake_provider import FakeSherbrooke
+from qiskit_ibm_runtime import Session, EstimatorV2 as Estimator
+from qiskit_aer import AerSimulator, Aer
+
+from qc_grader.challenges.iqc_2024 import (
+    grade_lab2_ex1,
+    grade_lab2_ex2,
+    grade_lab2_ex3,
+    grade_lab2_ex4,
+    grade_lab2_ex5
+)
+
+ans = {}
+
+# Place the correct letter next to the corresponding stage, inside a parenthesis
+# example:  ans["test"] = "M"
+
+ans["init"] = "C"
+ans["layout"] ="D"
+ans["routing"] ="B"
+ans["translation"] ="F"
+ans["optimization"] ="A"
+ans["scheduling"] ="E"
+# Submit your answer using following code
+
+grade_lab2_ex1(ans)
