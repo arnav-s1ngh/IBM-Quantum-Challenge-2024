2c71552ef34dbfeadee2b74ef8a4634db5255f05
Arnav Singh <92050142+arnav-s1ngh@users.noreply.github.com>
2024-06-13 08:38:48 +0530

    Update Exercise-1.py

--- Lab-3/Exercise-1.py
+++ Lab-3/Exercise-1.py
@@ -1,1 +1,57 @@
-#
+import matplotlib.pyplot as plt
+from qiskit import QuantumCircuit, transpile
+import os
+os.environ['QXToken'] = "Paste_Your_Token_Here"
+os.environ['QISKIT_IBM_TOKEN'] = "Paste_Your_Token_Here"
+
+import numpy as np
+from typing import List, Callable
+from scipy.optimize import minimize
+from scipy.optimize._optimize import OptimizeResult
+import matplotlib.pyplot as plt
+
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import Statevector, Operator, SparsePauliOp
+from qiskit.primitives import StatevectorSampler, PrimitiveJob
+from qiskit.circuit.library import TwoLocal
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit.visualization import plot_histogram
+from qiskit_ibm_runtime.fake_provider import FakeSherbrooke
+from qiskit_ibm_runtime import Session, EstimatorV2 as Estimator
+from qiskit_aer import AerSimulator, Aer
+
+from qc_grader.challenges.iqc_2024 import grade_lab3_ait_ex1, grade_lab3_ait_ex2
+
+from qiskit.circuit.random import random_circuit
+from qiskit.circuit.library import XGate, YGate
+from qiskit_ibm_runtime.fake_provider import FakeTorino, FakeOsaka
+from qiskit.transpiler import InstructionProperties, PassManager
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit.transpiler.preset_passmanagers.plugin import list_stage_plugins
+from qiskit.transpiler.timing_constraints import TimingConstraints
+from qiskit.transpiler.passes.scheduling import ASAPScheduleAnalysis,PadDynamicalDecoupling
+from qiskit.visualization.timeline import draw, IQXStandard
+from qiskit.transpiler import StagedPassManager
+from qiskit.visualization import plot_circuit_layout
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#Question-1
+
+from qiskit.circuit.library import EfficientSU2
+
+NUM_QUBITS = 61
+circuit = EfficientSU2(NUM_QUBITS, entanglement="circular", reps=1).decompose()
+
+print(f"Original circuit -> Depth: {circuit.depth()}, CNOTs: {circuit.num_nonlocal_gates()}")
+circuit.draw(fold=-1, output="mpl", style="iqp", scale=0.2)
+from qiskit_transpiler_service.transpiler_service import TranspilerService
+from qiskit_ibm_runtime import QiskitRuntimeService, Options
+transpiler_ai_false = TranspilerService(
+    backend_name="ibm_brisbane",
+    ai="false",
+    optimization_level=3
+)
+
+grade_lab3_ait_ex1(transpiler_ai_false)
